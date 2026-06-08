9e362297106822297a67123543d8cc6ceac5f1da
Aftab <aftab.a.tech24@gmail.com>
2026-06-08 20:27:04 +0530

    Add password validation logic
    Implement password validation to check length, number, and uppercase requirements.

---
+++ project1.py
@@ -0,0 +1,15 @@
+password=input("Enter your Password")
+has_number=False
+has_upper=False
+for ch in password:
+    if ch.isdigit():
+        has_number=True
+    if ch.isupper():
+        has_upper=True
+if (len(password)>=8 and has_number and has_upper):
+    print("password is updated")       
+else:
+    print("password must contain :")    
+    print("minimum 8 characters")
+    print("1 number")
+    print("1 uppercase")
